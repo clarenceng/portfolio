b73951f24deaf1c87319df5bae48aa18fb5eef8a
clarenceng <cng@live.ca>
2019-05-09 00:06:26 -0400

    converted to simple version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function init() {
   console.log('init')
-  anime({
-    targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800
-  });
+  // anime({
+  //   targets: 'div',
+  //   translateX: 250,
+  //   rotate: '1turn',
+  //   backgroundColor: '#FFF',
+  //   duration: 800
+  // });
 }
